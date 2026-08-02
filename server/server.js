4e6538b36adc74bd7c22116296c3f031be418764
Harshitha <harshitha280606@gmail.com>
2026-08-02 17:22:57 +0530

    Remove debug routes

--- server/server.js
+++ server/server.js
@@ -9,15 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const bookRoutes = require("./routes/bookRoutes");
-app.get("/test", (req, res) => {
-    res.json({ message: "Test route works" });
-});
-
-console.log("Book routes loaded");
-
-app.use("/api/books", bookRoutes);
-
-console.log("Routes registered");
 
 app.use("/api/books", bookRoutes);
 
