3808b3c33fb5c590d1d4626eece40a9a76bb5847
Harshitha <harshitha280606@gmail.com>
2026-08-02 17:18:54 +0530

    Add test route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 const bookRoutes = require("./routes/bookRoutes");
+app.get("/test", (req, res) => {
+    res.json({ message: "Test route works" });
+});
 
 console.log("Book routes loaded");
 
